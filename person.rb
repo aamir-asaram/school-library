c18f6a02834d1b9f625248e39445a45f27d9e2a6
Aamir <aamirasaram2107@gmail.com>
2023-08-28 14:55:03 +0200

    Fix linters: incorrect if and escape

--- person.rb
+++ person.rb
@@ -2,22 +2,20 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, parents_permission: true, name: "Unknown")
+  def initialize(age, parents_permission: true, name: 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-  end
-
-  private def of_age?
-    if @age >= 18
-      true
-    else
-      false
-    end
+    @parents_permission = parents_permission
   end
 
   def can_use_services?
     of_age? || @parents_permission
   end
 
+  private
+
+  def of_age?
+    @age && @age >= 18
+  end
 end
