eafd52027c27f8ab35a53cb083ffd2993b208196
Aamir <aamirasaram2107@gmail.com>
2023-08-28 16:27:33 +0200

    Fix linter errors

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,10 +1,6 @@
 require './decorator'
 
 class CapitalizeDecorator < Decorator
-  initialize(nameable)
-    super(nameable)
-  end
-
   def correct_name
     @nameable.correct_name.capitalize
   end
