bef269857c28f93c8798f752bac806d2446a231c
Aamir <aamirasaram2107@gmail.com>
2023-08-30 14:32:43 +0200

    Add a method that serves as the 'belongs-to' side (a student belongs to a classroom)

--- student.rb
+++ student.rb
@@ -3,8 +3,11 @@ require './person'
 class Student < Person
   def initialize(age, classroom, parents_permission: true, name: 'Unknown')
     super(age, parents_permission: parents_permission, name: name)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.add_student(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
